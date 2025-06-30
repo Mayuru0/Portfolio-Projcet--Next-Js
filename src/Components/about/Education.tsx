08a6d031c76cad99166efe37d529038913555c73
Mayuru Madhuranga <mayurumaduranga@gmail.com>
2025-06-30 21:43:41 +0530

    bugfix

--- src/Components/about/Education.tsx
+++ src/Components/about/Education.tsx
@@ -1,47 +1,7 @@
 import Link from "next/link";
 import React from "react";
+import{CertificationsDetails,educationDetails} from "@/Data/Education"
 
-const educationDetails = [
-  {
-    year: "2021-Present",
-    title: "Higher National Diploma in Information Technology",
-    institute: "Advanced Technological Institute-Gampaha",
-    link: "http://gampaha.sliate.ac.lk/",
-  },
-  {
-    year: "2019",
-    title: "G.C.E. A/L Examination",
-    institute: "Obtained 1C & 2S passes in Art Stream",
-    link: "#",
-  },
-];
-
-const CertificationsDetails = [
-  {
-    year: "2021",
-    title: "Certificate course in English",
-    institute: "Discovery Educational Centre Kiridiwela",
-    link: "",
-  },
-  {
-    year: "2019",
-    title: "Certificate Course in Graphics Designing",
-    institute: "Wijeya Graphics Gampaha",
-    link: "#",
-  },
-  {
-    year: "2019",
-    title: "Certificate Course in Graphics Designing",
-    institute: "Wijeya Graphics Gampaha",
-    link: "#",
-  },
-  {
-    year: "2016",
-    title: "NVQ Level 1 Certificate in National Youth Corps",
-    institute: "National Youth Corps Yakkala",
-    link: "#",
-  },
-];
 
 const Education = () => {
   return (
---
+++ src/Data/Education.ts
@@ -0,0 +1,48 @@
+export const educationDetails = [
+  {
+    year: "2021-Present",
+    title: "Higher National Diploma in Information Technology",
+    institute: "Advanced Technological Institute-Gampaha",
+    link: "http://gampaha.sliate.ac.lk/",
+  },
+  {
+    year: "2019",
+    title: "G.C.E. A/L Examination",
+    institute: "Obtained 1C & 2S passes in Art Stream",
+    link: "#",
+  },
+];
+
+export const CertificationsDetails = [
+  {
+    year: "2021",
+    title: "Certificate course in English",
+    institute: "Discovery Educational Centre Kiridiwela",
+    link: "",
+  },
+  {
+    year: "2019",
+    title: "Certificate Course in Graphics Designing",
+    institute: "Wijeya Graphics Gampaha",
+    link: "#",
+  },
+  {
+    year: "2016",
+    title: "NVQ Level 1 Certificate in National Youth Corps",
+    institute: "National Youth Corps Yakkala",
+    link: "#",
+  },
+  {
+    year: "2016",
+    title: "Ledership & Personality Development Training in National Youth Corps Yakkala",
+    institute: "National Youth Corps Yakkala",
+    link: "#",
+  },
+   {
+    year: "2016",
+    title: "Adventure-Based Training in Naula",
+    institute: "National Youth Corps ",
+    link: "#",
+  },
+  
+];
