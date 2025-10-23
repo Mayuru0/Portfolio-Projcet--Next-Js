481e9b9fde375e4440acf71e2a875a31d9af2509
Mayuru Madhuranga <mayurumaduranga@gmail.com>
2025-10-23 14:48:47 +0530

    like set

--- src/Components/pages/profile.tsx
+++ src/Components/pages/profile.tsx
@@ -62,8 +62,7 @@ const Profile: React.FC = () => {
         {/* Download CV Button */}
         <div className="my-8" data-aos="fade-in" data-aos-duration="2000">
           <a
-            href="https://drive.google.com/file/d/1i3z1REIei_6Np60xjcDzNQ8Mi9h6HWMU/view?usp=sharing"
-            download
+            href="https://drive.google.com/uc?export=download&id=1Ybhza03nVoCHKLl1x9k7lasQY_2BdZKB"
             className="px-4 py-2 sm:px-6 sm:py-3 w-[150px] sm:w-[200px] rounded-xl mr-4 border border-gray-400 hover:bg-gradient-to-br from-cyan-500 to-blue-500 hover:from-cyan-700 hover:to-blue-700 text-white hover:border-none flex items-center justify-center space-x-2 text-xs sm:text-sm md:text-base"
           >
             <FaDownload size={16} />
