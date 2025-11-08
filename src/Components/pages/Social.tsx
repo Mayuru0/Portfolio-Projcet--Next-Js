c28a82939366cb4d784a3743e9848946bbe6c394
Mayuru Madhuranga <164411973+Mayuru0@users.noreply.github.com>
2025-11-08 21:58:18 +0530

    Update Social.tsx

--- src/Components/pages/Social.tsx
+++ src/Components/pages/Social.tsx
@@ -11,7 +11,7 @@ const Social: React.FC<SocialProps> = ({ Cstyle }) => {
     { icon: <FaSquareFacebook className='text-[#316FF6] w-8 h-8 rounded-lg hover:scale-110 transition-transform duration-300 hover:opacity-80' />, path: "https://www.facebook.com/profile.php?id=100080722121368" },
     { icon: <FaSquareInstagram className='bg-gradient-to-r from-[#833ab4] via-[#fd1d1d] to-[#fcb045] w-8 h-8 rounded-lg hover:scale-110 transition-transform duration-300 hover:opacity-80' />, path: "https://www.instagram.com/_mayuru_madhuranga_/" },
     { icon: <FaLinkedin className='text-[#0077b5] w-8 h-8 rounded-lg hover:scale-110 transition-transform duration-300 hover:opacity-80' />, path: "https://www.linkedin.com/in/mayuru-madhuranga-7bbb73312/" },
-    { icon: <FaYoutube className='text-[#FF0000] w-9 h-9 hover:scale-110 transition-transform duration-300 hover:opacity-80' />, path: "https://www.youtube.com/@0_madhuranga_0" },
+    { icon: <FaYoutube className='text-[#FF0000] w-9 h-9 hover:scale-110 transition-transform duration-300 hover:opacity-80' />, path: "https://www.youtube.com/@MadhurangaOfficial" },
     { icon: <FaTwitterSquare className='text-[#1C9CEA] w-8 h-8 hover:scale-110 transition-transform duration-300 hover:opacity-80' />, path: "https://x.com/00_Marsh_00" },
     { icon: <FaSquareGithub className='text-white w-8 h-8 hover:scale-110 transition-transform duration-300 hover:opacity-80' />, path: "https://github.com/Mayuru0" },
   ];
