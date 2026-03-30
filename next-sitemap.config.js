a4a19c7bbe6bd0d09d586e012a85c2736795d1ff
Mayuru Madhuranga <mayurumaduranga@gmail.com>
2026-03-30 20:37:16 +0530

    set site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://mayuru-portfolio.vercel.app/', // ඔබේ domain එක
+  siteUrl: 'https://www.mayurumadhuranga.ooguy.com', // ඔබේ domain එක
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
